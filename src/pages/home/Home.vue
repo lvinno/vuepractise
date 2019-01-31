6d81f1da5d0a8f2fbc5248a574437983769a1cd3
lvino <553269233@qq.com>
2019-01-31 17:15:08 +1100

    create

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>Home</div>
+    <router-link to="/List">list</router-link>
+  </div>
+</template>
+<script>
+import HomeHeader from './components/HomeHeader'
+export default {
+  name: 'Home',
+  components: { HomeHeader },
+  data: function () {
+    return {}
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>List</div>
+</template>
+<script>
+export default {
+  name: 'List',
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+
+</style>
