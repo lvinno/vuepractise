ff44a8ddd985ea727796710fdc6c7b2ea5f822c4
lvino <553269233@qq.com>
2019-02-02 21:42:54 +1100

    change swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
     <router-link to="/List">list</router-link>
   </div>
 </template>
 <script>
 import HomeHeader from './components/HomeHeader'
+import HomeSwiper from './components/HomeSwiper'
 export default {
   name: 'Home',
   components: {
-     HomeHeader 
+     HomeHeader, 
+     HomeSwiper
      },
   data: function () {
     return {}
