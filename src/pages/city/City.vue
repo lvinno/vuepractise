5e9a9c20cd7579a676137b29d2055920484eecb5
lvino <553269233@qq.com>
2019-02-11 20:44:26 +1100

    city ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hot='hotCities'></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
@@ -12,15 +12,39 @@ import CityHeader from './components/CityHeader'
 import CitySearch from './components/CitySearch'
 import CityList from './components/CityList'
 import CityAlphabet from './components/CityAlphabet'
-
+import axios from 'axios'
 export default {
 name:'City',
+data(){
+  return {
+    cities:{},
+    hotCities:[]
+  }
+}
+,
 components:{
   CityHeader,
   CitySearch,
   CityList,
   CityAlphabet
-}  
+} ,
+mounted: function(){
+  this.getCityInfo()
+},
+methods: {
+  getCityInfo(){
+    axios.get('/static/mock/city.json')
+    .then(this.handleGetCityInfoSucc)
+  },
+  handleGetCityInfoSucc(res){
+    res = res.data;
+    if(res.ret && res.data){
+      const data = res.data;
+      this.cities = data.cities;
+      this.hotCities = data.hotCities;
+    }
+  }
+}
 }
 </script>
 
